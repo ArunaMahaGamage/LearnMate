c4d90623f174da7bc0d9a8786c1e055afd013a48
Aruna <admgbit@gmail.com>
2025-12-18 12:42:42 +0530

    resources

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initFirebase();
   await initHive();
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(const ProviderScope(child: App()));
 }
